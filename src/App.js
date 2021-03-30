dace81081817233e8ff7c1fd70e8c16de808df76
alitemel89 <alitemel89.ce@gmail.com>
2021-03-30 12:19:00 +0300

    Update App.css, add markers, add popup functionality

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+
+import * as parkData from "./data/skateparks.json";
 
 function App() {
+  const [activePark, setActivePark] = React.useState(null);
+  console.log(activePark);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>React Leaflet Demo</h1>
+      <MapContainer center={[45.421532, -75.697189]} zoom={12}>
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {parkData.features.map((park) => {
+          return (
+            <Marker
+              key={park.properties.PARK_ID}
+              position={[
+                park.geometry.coordinates[1],
+                park.geometry.coordinates[0],
+              ]}
+              eventHandlers={{
+                click: () => {
+                  setActivePark(park);
+                },
+              }}
+            />
+          );
+        })}
+        {activePark && (
+          <Popup
+            position={[
+              activePark.geometry.coordinates[1],
+              activePark.geometry.coordinates[0],
+            ]}
+            eventHandlers={{
+              close: () => {
+                setActivePark(null);
+              },
+            }}
+          >
+            <div>
+              <h2>{activePark.properties.NAME}</h2>
+              <p>{activePark.properties.DESCRIPTIO}</p>
+            </div>
+          </Popup>
+        )}
+      </MapContainer>
+    </>
   );
 }
 
